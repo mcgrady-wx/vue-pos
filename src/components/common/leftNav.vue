b42ee63708fa8966a010978989bbb80939d171ca
wen <380198622@qq.com>
2020-10-16 17:04:08 +0800

    first commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="leftNav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-tubiao-"></i>
+                <div>收银</div>
+            </li>
+
+            <li>
+                <i class="icon iconfont icon-dianpu"></i>
+                <div>店铺</div>
+            </li>
+
+            <li>
+                <i class="icon iconfont icon-food"></i>
+                <div>商品</div>
+            </li>
+
+            <li>
+                <i class="icon iconfont icon-huiyuan-"></i>
+                <div>会员</div>
+            </li>
+
+
+            <li>
+                <i class="icon iconfont icon-tongji"></i>
+                <div>统计</div>
+            </li>
+        </ul>       
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.leftNav{
+    height: 100%;
+    width: 5%;
+    float: left;
+    background: #1D8ce0;
+    font-size: 10px;
+    color: #fff;
+}
+.iconfont{
+    font-size:24px;
+}
+.leftNav ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.leftNav ul li{
+    text-align: center;
+    border-bottom:1px solid #20a0ff;
+    padding:10px;
+}
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="pos">
+        <el-row>
+            <el-col :span="7" id="order-list">我是订单栏</el-col>
+            <el-col :span="17">我是产品栏</el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        //解决用Element无法设置高度100%的问题
+        let winHeight=document.body.clientHeight
+        document.getElementById('order-list').style.height=winHeight+'px'
+    },
+}
+</script>
+
+<style>
+#order-list{
+    background: chartreuse;
+}
+</style>
